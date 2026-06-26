93bbf54e44614a383d17b4c8af17719b41e8d648
Znackt <Znackt@proton.me>
2026-06-26 10:04:24 +0530

    giest font issue fix

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
    output: 'standalone',
-   font: {
-    disable: true, // Skip next/font optimization during build
-  }
 };
 
 export default nextConfig;
